2c87a2b98399927d5ea243ac78a83cf9a84b0c3e
bk <bkang001@gmail.com>
2025-01-31 10:30:43 -0700

    zxcv

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Documentation',
   tagline: 'Documentation',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Set the production url of your site here
   url: 'https://docs.hexnibble.org',
